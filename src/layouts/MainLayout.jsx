ab30ad3f5617320478a60b8286308ecb7d0c983b
Chinh (KC) Do <contact@doinby.co>
2025-03-23 12:00:23 +1100

    fix nav bar not display on non-index pages

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
+import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router';
 
 export default function MainLayout() {
 	return (
 		<>
+			<Navbar />
 			<Outlet />
 		</>
 	);
